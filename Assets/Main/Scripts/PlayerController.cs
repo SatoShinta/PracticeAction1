dda51beecb5fd58f7a061c6a2256990b10cd646e
SatoShinta <kanten3005@gmail.com>
2024-11-25 15:11:04 +0900

    とりあえず縦横に動くようにした

--- Assets/Main/Scripts/PlayerController.cs
+++ Assets/Main/Scripts/PlayerController.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField, Header("動くスピード")] float moveSpeed = 10f;
+    Rigidbody playerRigit = null;
+    Vector3 movingVelocity = Vector3.zero;
+
     void Start()
     {
-        
+        playerRigit = this.gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var Vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movingDirection = new Vector3(horizontal, 0, Vertical).normalized;
+
+        movingVelocity = movingDirection * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigit.AddForce(movingVelocity);
     }
 }
