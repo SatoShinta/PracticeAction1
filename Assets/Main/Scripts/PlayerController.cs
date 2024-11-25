5e29f2647e946ac954fe755788c81f270158026e
SatoShinta <kanten3005@gmail.com>
2024-11-25 15:58:14 +0900

    簡単な移動処理を完成させた

--- Assets/Main/Scripts/PlayerController.cs
+++ Assets/Main/Scripts/PlayerController.cs
@@ -16,13 +16,25 @@ public class PlayerController : MonoBehaviour
         var horizontal = Input.GetAxisRaw("Horizontal");
         var Vertical = Input.GetAxisRaw("Vertical");
 
+        // 移動方向を計算（normalizedで正規化し、斜め移動でスピードが上がらないようにしている）
         Vector3 movingDirection = new Vector3(horizontal, 0, Vertical).normalized;
 
+        // 移動方向に速度をかけて移動速度を計算
         movingVelocity = movingDirection * moveSpeed;
+
+        // プレイヤーが移動しているとき、
+        if (movingVelocity != Vector3.zero)
+        {
+            // プレイヤーの正面を移動方向と同じにする
+            Quaternion targetRotation = Quaternion.LookRotation(movingDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.1f);
+        }
+
     }
 
     private void FixedUpdate()
     {
-        playerRigit.AddForce(movingVelocity);
+        // 移動処理
+        playerRigit.AddForce(movingVelocity, ForceMode.Force);
     }
 }
