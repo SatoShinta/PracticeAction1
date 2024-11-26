2471bf0a2e026b7b0cdbee9fb71efbf4bbd55b6e
SatoShinta <kanten3005@gmail.com>
2024-11-26 18:25:33 +0900

    プレイヤーの歩き移動はできた

--- Assets/Main/Scripts/PlayerController.cs
+++ Assets/Main/Scripts/PlayerController.cs
@@ -5,10 +5,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Header("動くスピード")] float moveSpeed = 10f;
     Rigidbody playerRigit = null;
     Vector3 movingVelocity = Vector3.zero;
+    Animator playerAnim = null;
 
     void Start()
     {
         playerRigit = this.gameObject.GetComponent<Rigidbody>();
+        playerAnim = this.gameObject.GetComponent<Animator>();
     }
 
     void Update()
@@ -21,6 +23,7 @@ public class PlayerController : MonoBehaviour
 
         // 移動方向に速度をかけて移動速度を計算
         movingVelocity = movingDirection * moveSpeed;
+        playerRigit.linearVelocity = new Vector3(movingVelocity.x, movingVelocity.y, movingVelocity.z);
 
         // プレイヤーが移動しているとき、
         if (movingVelocity != Vector3.zero)
@@ -28,7 +31,15 @@ public class PlayerController : MonoBehaviour
             // プレイヤーの正面を移動方向と同じにする
             Quaternion targetRotation = Quaternion.LookRotation(movingDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.1f);
+            playerAnim.SetBool("isWalking", true);
         }
+        else
+        {
+            playerRigit.linearVelocity = Vector3.zero;
+            playerRigit.angularVelocity = Vector3.zero;
+            playerAnim.SetBool("isWalking", false);
+        }
+
 
     }
 
