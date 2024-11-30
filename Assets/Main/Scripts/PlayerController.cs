705ceb7bc320d0d2c59641b7bdf400e912323073
SatoShinta <kanten3005@gmail.com>
2024-11-30 19:48:52 +0900

    階段をするする上がれるようになった（降りられはしない）

--- Assets/Main/Scripts/PlayerController.cs
+++ Assets/Main/Scripts/PlayerController.cs
@@ -4,6 +4,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField, Header("動くスピード")] float moveSpeed = 10f;
     [SerializeField, Header("走ってる時のスピード")] float runSpeed = 10f;
+    [SerializeField, Header("レイを飛ばす位置")] Transform stepRay;
+    [SerializeField, Header("例を飛ばす距離")] float stepDistance = 0.5f;
+    [SerializeField, Header("登れる段差")] float stepOffset = 0.3f;
+    [SerializeField, Header("登れる角度")] float slopeLimit = 0.3f;
+    [SerializeField, Header("登れる段差の位置から飛ばすレイの距離")] float slopeDistance = 0.3f;
+    public int layerMask;
+
     Rigidbody playerRigit = null;
     Vector3 movingVelocity = Vector3.zero;
     Vector3 cameraDirection = Vector3.zero;
@@ -14,10 +21,12 @@ public class PlayerController : MonoBehaviour
     {
         playerRigit = this.gameObject.GetComponent<Rigidbody>();
         playerAnim = this.gameObject.GetComponent<Animator>();
+        layerMask = ~(1 << LayerMask.NameToLayer("Player"));
     }
 
     void Update()
     {
+        // 入力を受けつけ
         var horizontal = Input.GetAxisRaw("Horizontal");
         var Vertical = Input.GetAxisRaw("Vertical");
 
@@ -40,6 +49,18 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.1f);
             playerAnim.SetBool("isWalking", true);
 
+            // 登れる段差を表示
+            Debug.DrawLine(transform.position + new Vector3(0f, stepOffset, 0f), transform.position + new Vector3(0f, stepOffset, 0f) + transform.forward * slopeDistance, Color.green);
+            if(Physics.Linecast(transform.position,stepRay.position +stepRay.forward * stepDistance, out var stepHit, LayerMask.GetMask("Field", "Block")))
+            {
+                if (Vector3.Angle(transform.up, stepHit.normal) <= slopeLimit
+                    || (Vector3.Angle(transform.up, stepHit.normal) > slopeLimit
+                    && !Physics.Linecast(transform.position + new Vector3(0f, stepOffset, 0f), transform.position + new Vector3(0f, stepOffset, 0f) + transform.forward * slopeDistance, LayerMask.GetMask("Field", "Block"))))
+                {
+                    movingVelocity = new Vector3(0f, (Quaternion.FromToRotation(Vector3.up, stepHit.normal) * transform.forward * moveSpeed).y) + transform.forward * moveSpeed;
+                    Debug.Log(Vector3.Angle(transform.up, stepHit.normal));
+                }
+            }
 
 
             // 走りモーション
@@ -69,4 +90,7 @@ public class PlayerController : MonoBehaviour
         // 移動処理
         playerRigit.linearVelocity = new Vector3(movingVelocity.x, movingVelocity.y, movingVelocity.z);
     }
+
+
+
 }
