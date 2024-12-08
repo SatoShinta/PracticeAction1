056d01f5cc2989cd4df8f3d3c669b6e32d5a5e04
SatoShinta <kanten3005@gmail.com>
2024-12-08 21:47:12 +0900

    敵のダメージ処理関係を追加した（仮）

---
+++ Assets/Main/Scripts/EnemyScript/EnemyDamageManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class EnemyDamageManager : MonoBehaviour
+{
+   [SerializeField,Header("ƒ_ƒ[ƒW‚ðŽó‚¯‚½‰ñ”")] int damageCounter = 0;
+
+    private void Update()
+    {
+        if(damageCounter > 5)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PlayerAttackCollider")
+        {
+            damageCounter++;
+        }
+    }
+
+
+}
