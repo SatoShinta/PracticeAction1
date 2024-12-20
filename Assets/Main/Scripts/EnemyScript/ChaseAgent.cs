b311f9fc3beb98f602edebb06438d7f0cc6be0e2
SatoShinta <kanten3005@gmail.com>
2024-12-20 21:41:54 +0900

    何とか力業で直った... とりあえず敵の判定にPhisics.SphereColliderは使えないことが分かった（原因不明） 別の方法を探す

--- Assets/Main/Scripts/EnemyScript/ChaseAgent.cs
+++ Assets/Main/Scripts/EnemyScript/ChaseAgent.cs
@@ -3,44 +3,68 @@ using UnityEngine.AI;
 
 public class ChaseAgent : MonoBehaviour
 {
-    [SerializeField] GameObject player;
+    [SerializeField] Transform player;
     [SerializeField] float rad = 0f;
     [SerializeField] float attackRad = 0f;
+    [SerializeField] Vector3 rootPos = Vector3.zero; // 初期位置
     NavMeshAgent agent;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.enabled = false;
+        agent.enabled = true;
+        rootPos = transform.position;
     }
 
     void Update()
     {
        // CheckPlayer();
-        if (agent.enabled)
-        {
-            agent.destination = player.transform.position;
-        }
     }
 
     //public void CheckPlayer()
     //{
     //    if (Physics.CheckSphere(transform.position, rad, LayerMask.GetMask("Player")))
     //    {
-    //        transform.LookAt(player.transform);
-    //        agent.enabled = true;
+    //        player = FindAnyObjectByType<PlayerController>().transform;
+    //        agent.destination = player.position;
+    //        Debug.Log("みつけた" + player.position);
     //    }
     //    else
     //    {
-    //        agent.enabled = false;
+    //        player = default;
+    //        //playerが範囲の外に出たら初期位置に戻る
+    //        agent.destination = rootPos;
+    //        Debug.Log("どっか行っちゃった");
     //    }
     //}
 
 
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = FindAnyObjectByType<PlayerController>().transform;
+            agent.destination = player.position;
+            Debug.Log("みつけた" + player.position);
+        }
+        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = default;
+            //playerが範囲の外に出たら初期位置に戻る
+            agent.destination = rootPos;
+            Debug.Log("どっか行っちゃった");
+        }
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position ,rad);
+        Gizmos.DrawWireSphere(transform.position, rad);
         Gizmos.DrawSphere(transform.position, attackRad);
     }
 }
